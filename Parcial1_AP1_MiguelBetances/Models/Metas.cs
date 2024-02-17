7dbc9afbd95443c1e7e172026fce86dc8604205a
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-02-16 20:09:06 -0400

    Remodelando la clase Metas.

--- Parcial1_AP1_MiguelBetances/Models/Metas.cs
+++ Parcial1_AP1_MiguelBetances/Models/Metas.cs
@@ -9,12 +9,8 @@ namespace Parcial1_AP1_MiguelBetances.Models
 
         public DateTime Fecha { get; set; } = DateTime.Now;
 
-        [Required(ErrorMessage = "Es obligatorio introducir la Persona")]
-        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "El nombre solo debe contener letras y espacios.")]
-        public string? Persona { get; set; }
-
         [Required(ErrorMessage = "Es obligatorio introducir la Observación")]
-        public string? Observacion { get; set; }
+        public string? Descripcion { get; set; }
 
         [Required(ErrorMessage = "Es obligatorio introducir el Monto")]
         [Range(1, 7922816251426433759, ErrorMessage = "El monto debe ser mayor que cero")]
